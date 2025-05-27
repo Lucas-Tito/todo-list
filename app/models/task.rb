e26c0f6ab0a94eee851ef37bc5f3b705a755ecad
Lucas Tito <lucassga500@gmail.com>
2025-05-27 10:15:17 -0300

    fix: fix task.rb error asking for more parameters

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
-  enum priority: { low: 0, medium: 1, high: 2 } # Exemplo de prioridades
-
+  enum :task_priority, { low: 0, medium: 1, high: 2 } # SÍMBOLO + HASH
+  
   validates :title, presence: true
   # Adicione outras validações conforme necessário
 
