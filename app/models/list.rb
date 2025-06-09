59c061e60589836151a66de4baf544058e930bd7
Lucas Tito <lucassga500@gmail.com>
2025-06-09 11:29:53 -0300

    feat: list model now has position property

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,14 @@
 class List < ApplicationRecord
-    has_many :tasks, dependent: :destroy
+  has_many :tasks, dependent: :destroy
+  validates :name, presence: true
 
-    validates :name, presence: true
+  before_create :set_position
+
+  default_scope { order(position: :asc) }
+
+  private
+
+  def set_position
+    self.position = List.maximum(:position).to_i + 1
+  end
 end
