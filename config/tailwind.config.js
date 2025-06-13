b403e4e88f837825e0c7b34d40e1dea41659f41b
Lucas Tito <lucassga500@gmail.com>
2025-06-13 14:28:16 -0300

    refactor: config color variables

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,5 +7,20 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        'primary-hover': 'rgb(var(--color-primary-hover) / <alpha-value>)',
+        'secondary-hover': 'rgb(var(--color-primary-hover) / <alpha-value>)',
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
   ]
 }
